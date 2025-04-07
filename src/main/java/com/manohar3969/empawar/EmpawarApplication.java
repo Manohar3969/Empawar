f12a331d90b676fa75d1f20d0301229172e7bb8b
Manohar V <reddymanohar3969@gmail.com>
2025-04-07 12:29:05 +0530

    Initial commit - Empawar

---
+++ src/main/java/com/manohar3969/empawar/EmpawarApplication.java
@@ -0,0 +1,13 @@
+package com.manohar3969.empawar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpawarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpawarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manohar3969/empawar/EmpawarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manohar3969.empawar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpawarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
